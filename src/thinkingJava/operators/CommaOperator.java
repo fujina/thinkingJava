56aadba93f88dc4ef974625e57242b0e8a981f95
fujina <37600009+fujina@users.noreply.github.com>
2018-04-04 21:52:21 +0800

    4.4night
    4.4night

---
+++ src/thinkingJava/operators/CommaOperator.java
@@ -0,0 +1,19 @@
+//: operator/CommaOperator.java
+
+package thinkingJava.operators;
+
+public class CommaOperator {
+
+	public static void main(String[] args) {
+		for(int i = 1, j = 1 + 10; i< 5; i++,j=i*2) {
+			System.out.println("i= " + i + " j = " +j);
+		}
+
+	}
+
+}/*output:
+i= 1 j = 11
+i= 2 j = 4
+i= 3 j = 6
+i= 4 j = 8
+*/
