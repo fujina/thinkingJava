eb22c3ba61567adeac38185c1eff5bc77b6c1f56
fujina <37600009+fujina@users.noreply.github.com>
2018-04-11 08:02:27 +0800

    4.11~
    4.11~

---
+++ src/thinkingJava/control/LabeledFor.java
@@ -0,0 +1,46 @@
+//: control/LabeledFor.java
+
+package thinkingJava.control;
+
+import static net.mindview.util.Print.*;
+
+public class LabeledFor {
+
+	public static void main(String[] args) {
+		int i = 0;
+		outer: // Can't have statements here
+		for (; true;) {
+			inner: for (; i < 10; i++) {
+				print("i = " + i);
+				if (i == 2) {
+					print("continue");
+					continue;
+				}
+				if (i == 3) {
+					print("break");
+					i++;
+					break;
+				}
+				if (i == 7) {
+					print("continue outer");
+					i++;
+					continue outer;
+				}
+				if (i == 8) {
+					print("break outer");
+					break outer;
+				}
+				for (int k = 0; k < 5; k++) {
+					if (k == 3) {
+						print("continue inner");
+						continue inner;
+					}
+				}
+			}
+		}
+	}
+}/*
+	 * output: i = 0 continue inner i = 1 continue inner i = 2 continue i = 3 break
+	 * i = 4 continue inner i = 5 continue inner i = 6 continue inner i = 7 continue
+	 * outer i = 8 break outer
+	 */
