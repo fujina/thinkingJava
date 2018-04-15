b835af6e9d34cd1a50b1ee997b78c25f81d379b3
fujina <37600009+fujina@users.noreply.github.com>
2018-04-15 21:19:24 +0800

    4.15
    4.15

--- src/thinkingJava/initialization/SimpleConstructor2.java
+++ src/thinkingJava/initialization/SimpleConstructor2.java
@@ -11,9 +11,9 @@ class Rock2 {
 public class SimpleConstructor2 {
 
 	public static void main(String[] args) {
-		for (int i = 0; i < 8; i++)
+		for (int i = 0; i < 10; i++)
 			new Rock2(i);
 	}
 }/*output:
-Rock 0 Rock 1 Rock 2 Rock 3 Rock 4 Rock 5 Rock 6 Rock 7 
+Rock 0 Rock 1 Rock 2 Rock 3 Rock 4 Rock 5 Rock 6 Rock 7 Rock 8 Rock 9
 */
