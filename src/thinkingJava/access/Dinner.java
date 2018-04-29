14ea47ba1b9b58507910aadde54dad33f15fb5dc
fujina <37600009+fujina@users.noreply.github.com>
2018-04-29 12:57:29 +0800

    4.29
    4.29

---
+++ src/thinkingJava/access/Dinner.java
@@ -0,0 +1,13 @@
+// access/Dinner.java
+
+package thinkingJava.access;
+import thinkingJava.access.dessert.*;
+
+public class Dinner {
+
+	public static void main(String[] args) {
+		Cookie x = new Cookie();
+		x.bite();
+	}
+
+}
---
+++ src/thinkingJava/access/dessert/Cookie.java
@@ -0,0 +1,13 @@
+//: access/dessert//Cookie.java
+
+package thinkingJava.access.dessert;
+
+public class Cookie {
+	public Cookie() {
+		System.out.println("Cookie constructor");
+	}
+
+	public void bite() {
+		System.out.println("bite");
+	}
+}
