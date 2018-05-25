4a769b155ff6b8d968664b382bb02b423f638c71
fujina <895156130@qq.com>
2018-05-25 14:43:19 +0800

    5.25
    5.25

---
+++ src/thinkingJava/interfaces/filters/HighPass.java
@@ -0,0 +1,16 @@
+//: interfaces/filters/HighPass.java
+
+package thinkingJava.interfaces.filters;
+
+public class HighPass extends Filter {
+	double cutoff;
+
+	public HighPass(double cutoff) {
+		this.cutoff = cutoff;
+	}
+
+	public Waveform process(Waveform input) {
+		return input;
+	}
+
+}
