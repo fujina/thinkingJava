3e0825c1bbdd25b3dd32b8dae6e808c3b3bbcde1
fujina <895156130@qq.com>
2018-05-28 14:11:23 +0800

    5.28
    5.28

---
+++ src/thinkingJava/interfaces/TestRandVals.java
@@ -0,0 +1,20 @@
+//: interfaces/TestRandVals.java
+
+package thinkingJava.interfaces;
+
+import static net.mindview.util.Print.*;
+
+public class TestRandVals {
+
+	public static void main(String[] args) {
+		print(RandVals.RANDOM_INT);
+		print(RandVals.RANDOM_LONG);
+		print(RandVals.RANDOM_FLOAT);
+		print(RandVals.RANDOM_DOUBLE);
+	}
+}/*output:
+8
+-32032247016559954
+-8.5939291E18
+5.779976127815049
+*/
