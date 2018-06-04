4f0890f411d01ccba1ba810afaf174a88e4ad6de
fujina <895156130@qq.com>
2018-06-04 11:16:23 +0800

    6.4
    6.4

---
+++ src/thinkingJava/holding/AddingGroups.java
@@ -0,0 +1,19 @@
+//: holding/AddingGroups.java
+
+package thinkingJava.holding;
+
+import java.util.*;
+
+public class AddingGroups {
+
+	public static void main(String[] args) {
+		Collection<Integer> collection = new ArrayList<Integer>(Arrays.asList(1, 2, 3, 4, 5));
+		Integer[] moreInts = { 6, 7, 8, 6, 10 };
+		collection.addAll(Arrays.asList(moreInts));
+		Collections.addAll(collection, 11, 12, 13, 14, 15);
+		Collections.addAll(collection, moreInts);
+
+		List<Integer> list = Arrays.asList(16, 17, 18, 19, 20);
+		list.set(1, 99);
+	}
+}
---
+++ src/thinkingJava/holding/SimpleCollection.java
@@ -0,0 +1,18 @@
+//: holding/SimpleCollection.java
+
+package thinkingJava.holding;
+
+import java.util.*;
+
+public class SimpleCollection {
+
+	public static void main(String[] args) {
+		Collection<Integer> c = new ArrayList<Integer>();
+		for (int i = 0; i < 10; i++)
+			c.add(i);
+		for (Integer i : c)
+			System.out.print(i + "\t");
+
+	}
+
+}
