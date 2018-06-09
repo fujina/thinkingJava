532d933c124823309c5379ce750483efd3154c80
fujina <895156130@qq.com>
2018-06-09 13:09:53 +0800

    6.9
    6.9

---
+++ src/thinkingJava/holding/MultiIterableClass.java
@@ -0,0 +1,13 @@
+//: holding/MultiIterableClass.java
+
+package thinkingJava.holding;
+import java.util.*;
+
+public class MultiIterableClass extends IterableClass{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
