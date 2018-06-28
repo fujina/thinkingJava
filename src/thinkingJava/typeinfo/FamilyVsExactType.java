1237376ad0c61663b944f6967901b55e97254a22
fujina <895156130@qq.com>
2018-06-28 16:55:26 +0800

    6.28
    6.28

---
+++ src/thinkingJava/typeinfo/FamilyVsExactType.java
@@ -0,0 +1,46 @@
+//: typeinfo/FamilyVsExactType.java
+
+package thinkingJava.typeinfo;
+
+import static net.mindview.util.Print.*;
+
+class Base {
+}
+
+class Derived extends Base {
+}
+
+public class FamilyVsExactType {
+	static void test(Object x) {
+		print("Testing x of type " + x.getClass());
+		print("x instanceof Base " + (x instanceof Base));
+		print("x instance of Derived " + (x instanceof Derived));
+		print("Base. isInstance(x) " + Base.class.isInstance(x));
+		print("Derived.isInstance(x)" + Derived.class.isInstance(x));
+		print("x.getClass() == Base.class " + (x.getClass() == Base.class));
+		print("x.getClass().equals(Base.class)) " + (x.getClass().equals(Base.class)));
+		print("x.getClass().equals(Derived.class)) " + (x.getClass().equals(Derived.class)));
+	}
+
+	public static void main(String[] args) {
+		test(new Base());
+		test(new Derived());
+	}
+}/*output:
+Testing x of type class thinkingJava.typeinfo.Base
+x instanceof Base true
+x instance of Derived false
+Base. isInstance(x) true
+Derived.isInstance(x)false
+x.getClass() == Base.class true
+x.getClass().equals(Base.class)) true
+x.getClass().equals(Derived.class)) false
+Testing x of type class thinkingJava.typeinfo.Derived
+x instanceof Base true
+x instance of Derived true
+Base. isInstance(x) true
+Derived.isInstance(x)true
+x.getClass() == Base.class false
+x.getClass().equals(Base.class)) false
+x.getClass().equals(Derived.class)) true
+*/
