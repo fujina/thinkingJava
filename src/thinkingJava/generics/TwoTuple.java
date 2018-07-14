87e718ee15e744e4db5aa11aa28a1a06ff26956f
fujina <895156130@qq.com>
2018-07-14 12:27:06 +0800

    7.14
    7.14

---
+++ src/thinkingJava/generics/TwoTuple.java
@@ -0,0 +1,17 @@
+//: generics/TwoTuple.java
+
+package thinkingJava.generics;
+
+public class TwoTuple<A,B>{
+	public final A first;
+	public final B second;
+	public TwoTuple(A a, B b) {first = a; second = b;}
+	
+	public String toString() {
+		return "(" + first + ", " + second + ")";
+	}
+
+	public static void main(String[] args) {
+	}
+
+}
