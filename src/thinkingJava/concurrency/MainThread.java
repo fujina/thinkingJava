45db9c55dbd7dcfdbd675033249343483c6bd13c
fujina <895156130@qq.com>
2018-07-17 19:47:32 +0800

    7.17
    7.17

---
+++ src/thinkingJava/concurrency/MainThread.java
@@ -0,0 +1,23 @@
+//: concurrency/MainThread.java
+
+package thinkingJava.concurrency;
+
+public class MainThread {
+
+	public static void main(String[] args) {
+		LiftOff launch = new LiftOff();
+		launch.run();
+	}
+}/*output:
+#0(9).
+#0(8).
+#0(7).
+#0(6).
+#0(5).
+#0(4).
+#0(3).
+#0(2).
+#0(1).
+#0(Liftoff!).
+*/
+
