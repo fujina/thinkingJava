d42830ac42add95896d2c4675defdafac070044d
fujina <895156130@qq.com>
2018-07-21 20:00:11 +0800

    7.21
    7.21

---
+++ src/thinkingJava/concurrency/FixedThreadPool.java
@@ -0,0 +1,64 @@
+//concurrency/FixedThreadPool.java
+
+package thinkingJava.concurrency;
+
+import java.util.concurrent.*;
+
+public class FixedThreadPool {
+	public static void main(String[] args) {
+		ExecutorService exec = Executors.newFixedThreadPool(5);
+		for (int i = 0; i < 5; i++)
+			exec.execute(new LiftOff());
+	}
+}/*output:
+#1(9).
+#3(9).
+#1(8).
+#3(8).
+#1(7).
+#3(7).
+#1(6).
+#3(6).
+#1(5).
+#3(5).
+#1(4).
+#3(4).
+#1(3).
+#3(3).
+#1(2).
+#3(2).
+#1(1).
+#3(1).
+#1(Liftoff!).
+#3(Liftoff!).
+#0(9).
+#2(9).
+#4(9).
+#0(8).
+#2(8).
+#4(8).
+#0(7).
+#2(7).
+#4(7).
+#0(6).
+#2(6).
+#4(6).
+#0(5).
+#2(5).
+#4(5).
+#0(4).
+#2(4).
+#4(4).
+#0(3).
+#2(3).
+#4(3).
+#0(2).
+#2(2).
+#4(2).
+#0(1).
+#2(1).
+#4(1).
+#0(Liftoff!).
+#2(Liftoff!).
+#4(Liftoff!).
+*/
