fa19f0245c92f9cdfd44210120e4f5e6ccabcb08
fujina <895156130@qq.com>
2018-07-22 13:01:43 +0800

    7.22
    7.22

---
+++ src/thinkingJava/concurrency/SingleThreadExecutor.java
@@ -0,0 +1,16 @@
+//: concurrency/SingleThreadExecutor.java
+
+package thinkingJava.concurrency;
+
+import java.util.concurrent.*;
+
+public class SingleThreadExecutor {
+
+	public static void main(String[] args) {
+		ExecutorService exec = Executors.newSingleThreadExecutor();
+		for (int i = 0; i < 5; i++)
+			exec.execute(new LiftOff());
+
+	}
+
+}
